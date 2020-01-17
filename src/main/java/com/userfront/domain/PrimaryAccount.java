6778780a075fc14581ee446f6a47986793296ccf
Pavel <rogozhin.p@yandex.ru>
2020-01-17 15:58:27 +0300

    connect to database done

--- src/main/java/com/userfront/domain/PrimaryAccount.java
+++ src/main/java/com/userfront/domain/PrimaryAccount.java
@@ -3,8 +3,21 @@ package com.userfront.domain;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
 public class PrimaryAccount {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private int accountNumer;
 	private BigDecimal accountBalance;
@@ -41,6 +54,8 @@ public class PrimaryAccount {
 		this.primaryTransactionList = primaryTransactionList;
 	}
 
+	@OneToMany(mappedBy = "primaryAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JsonIgnore
 	private List<PrimaryTransaction> primaryTransactionList;
 
 }

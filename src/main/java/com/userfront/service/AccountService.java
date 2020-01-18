9ee58606d590c34e119be0a2d9cbb253bdb2e08d
Pavel <rogozhin.p@yandex.ru>
2020-01-18 16:11:50 +0300

    add withdraw

--- src/main/java/com/userfront/service/AccountService.java
+++ src/main/java/com/userfront/service/AccountService.java
@@ -12,4 +12,6 @@ public interface AccountService {
 
 	void deposit(String accountType, double amount, Principal principal);
 
+	void withdraw(String accountType, double amount, Principal principal);
+
 }

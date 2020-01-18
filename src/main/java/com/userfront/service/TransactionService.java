ef380958a2920c249ede7a18394d06c13cf5e222
Pavel <rogozhin.p@yandex.ru>
2020-01-18 20:00:54 +0300

    add transer between accounts and show transaction list on account's page

--- src/main/java/com/userfront/service/TransactionService.java
+++ src/main/java/com/userfront/service/TransactionService.java
@@ -2,7 +2,9 @@ package com.userfront.service;
 
 import java.util.List;
 
+import com.userfront.domain.PrimaryAccount;
 import com.userfront.domain.PrimaryTransaction;
+import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.SavingsTransaction;
 
 public interface TransactionService {
@@ -17,5 +19,8 @@ public interface TransactionService {
 	void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction);
 
 	void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction);
+
+	void betweenAccountsTransfer(String transferFrom, String transferTo, String amount, PrimaryAccount primaryAccount,
+			SavingsAccount savingsAccount) throws Exception;
 
 }

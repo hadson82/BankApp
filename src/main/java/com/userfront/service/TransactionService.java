e144815634907f09f2e614266c86cbb74e253546
Pavel <rogozhin.p@yandex.ru>
2020-01-19 11:50:34 +0300

    add transfer to someOneElse

--- src/main/java/com/userfront/service/TransactionService.java
+++ src/main/java/com/userfront/service/TransactionService.java
@@ -33,4 +33,7 @@ public interface TransactionService {
 
 	void deleteRecipientByName(String recipientName);
 
+	void toSomeoneElseTransfer(Recipient recipient, String accountType, String amount, PrimaryAccount primaryAccount,
+			SavingsAccount savingsAccount);
+
 }

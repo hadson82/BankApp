cb47ede1814016f5f58d0a282c586365cce65b8f
Pavel <rogozhin.p@yandex.ru>
2020-01-25 13:32:46 +0300

    fix 500 error

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.userfront.controller;
 
 import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.userfront.dao.RoleDao;
 import com.userfront.domain.PrimaryAccount;
 import com.userfront.domain.SavingsAccount;
 import com.userfront.domain.User;
+import com.userfront.domain.security.UserRole;
 import com.userfront.service.UserService;
 
 @Controller
@@ -20,8 +24,8 @@ public class HomeController {
 	@Autowired
 	private UserService userService;
 
-//	@Autowired
-//	private RoleDao roleDao;
+	@Autowired
+	private RoleDao roleDao;
 
 	@RequestMapping("/")
 	public String home() {
@@ -53,10 +57,10 @@ public class HomeController {
 			}
 			return "signup";
 		} else {
-//			Set<UserRole> userRoles = new HashSet<>();
-//			userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+			Set<UserRole> userRoles = new HashSet<>();
+			userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
 
-			userService.save(user);
+			userService.createUser(user, userRoles);
 
 			return "redirect:/";
 		}
--- src/main/java/com/userfront/dao/PrimaryAccountDao.java
+++ src/main/java/com/userfront/dao/PrimaryAccountDao.java
@@ -1,12 +1,10 @@
 package com.userfront.dao;
 
-import com.userfront.domain.PrimaryAccount;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by z00382545 on 10/21/16.
- */
-public interface PrimaryAccountDao extends CrudRepository<PrimaryAccount,Long> {
+import com.userfront.domain.PrimaryAccount;
 
-    PrimaryAccount findByAccountNumber (int accountNumber);
+public interface PrimaryAccountDao extends CrudRepository<PrimaryAccount, Long> {
+
+	PrimaryAccount findByAccountNumber(int accountNumber);
 }
